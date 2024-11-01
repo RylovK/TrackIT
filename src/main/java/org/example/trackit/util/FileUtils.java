d739d8666161fb725777d512fa36115cd1dbf441
krylov <rylov.k@gmail.com>
2024-11-01 16:46:25 +0400

    lower filter for certified controller - done,  TODO: export -> file utils;  implement default methods in equipment service interface  log all status changes

--- src/main/java/org/example/trackit/util/FileUtils.java
+++ src/main/java/org/example/trackit/util/FileUtils.java
@@ -2,18 +2,26 @@ package org.example.trackit.util;
 
 import jakarta.annotation.PostConstruct;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.example.trackit.dto.CertifiedEquipmentDTO;
+import org.example.trackit.dto.EquipmentDTO;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @Service
 public class FileUtils {
@@ -80,5 +88,91 @@ public class FileUtils {
         String extension = FilenameUtils.getExtension(originalFilename);
         String formattedDate = LocalDate.now().format(formatter);
         return String.format("%s_%s.%s", fileNameWithOutExt, formattedDate, extension);
+    }
+
+    public <T extends EquipmentDTO> OutputStream getOutputStream(List<T> equipmentList, Class<T> clazz) {
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("Equipment");
+        Row headerRow = sheet.createRow(0);
+        fillHeaders(headerRow);
+        if (clazz == CertifiedEquipmentDTO.class) {
+            fillCertifiedHeaders(headerRow);
+        }
+        int rowNum = 1;
+        for (T equipment : equipmentList) {
+
+        }
+        return null;
+    }
+
+    public Workbook getEquipmentWorkbook(List<EquipmentDTO> equipmentList) {
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("Equipment");
+        Row headerRow = sheet.createRow(0);
+        fillHeaders(headerRow);
+        int rowNum = 1;
+        for (EquipmentDTO dto : equipmentList) {
+            Row row = sheet.createRow(rowNum++);
+            fillDataToRow(row, dto);
+        }
+        return workbook;
+    }
+
+    public Workbook getCertifiedWorkbook(List<CertifiedEquipmentDTO> equipmentList) {
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("Certified Equipment");
+        Row headerRow = sheet.createRow(0);
+        fillCertifiedHeaders(headerRow);
+        int rowNum = 1;
+        for (CertifiedEquipmentDTO dto : equipmentList) {
+            Row row = sheet.createRow(rowNum++);
+            fillCertifiedDataToRow(row, dto);
+        }
+        return workbook;
+    }
+
+    private void fillCertifiedDataToRow(Row row, CertifiedEquipmentDTO dto) {
+        fillDataToRow(row, dto);
+        row.createCell(10).setCellValue(dto.getCertificationStatus().toString());
+        row.createCell(11).setCellValue(dto.getCertificationDate().toString());
+        row.createCell(12).setCellValue(dto.getCertificationPeriod());
+        row.createCell(13).setCellValue(dto.getNextCertificationDate().toString());
+    }
+
+    private void fillCertifiedHeaders(Row headerRow) {
+        fillHeaders(headerRow);
+        headerRow.createCell(10).setCellValue("Certification status");
+        headerRow.createCell(11).setCellValue("Certification date");
+        headerRow.createCell(12).setCellValue("Certification period");
+        headerRow.createCell(13).setCellValue("Next certification date");
+    }
+
+    private void fillDataToRow(Row row, EquipmentDTO dto) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        row.createCell(0).setCellValue(dto.getId());
+        row.createCell(1).setCellValue(dto.getSerialNumber());
+        row.createCell(2).setCellValue(dto.getPartNumber());
+        row.createCell(3).setCellValue(dto.getDescription());
+        row.createCell(4).setCellValue(dto.getHealthStatus().toString());
+        row.createCell(5).setCellValue(dto.getAllocationStatus().toString());
+        row.createCell(6).setCellValue(dto.getJobName());
+        row.createCell(7).setCellValue(dto.getLastJob());
+        row.createCell(8).setCellValue(dto.getAllocationStatusLastModified() == null ? null : dto.getAllocationStatusLastModified().toString());
+        row.createCell(9).setCellValue(dto.getComments());
+        row.createCell(10).setCellValue(dto.getCreatedAt().format(formatter));
+    }
+
+    private void fillHeaders(Row headerRow) {
+        headerRow.createCell(0).setCellValue("ID");
+        headerRow.createCell(1).setCellValue("Serial Number");
+        headerRow.createCell(2).setCellValue("Part Number");
+        headerRow.createCell(3).setCellValue("Description");
+        headerRow.createCell(4).setCellValue("Health Status");
+        headerRow.createCell(5).setCellValue("Allocation Status");
+        headerRow.createCell(6).setCellValue("Job name");
+        headerRow.createCell(7).setCellValue("Last job name");
+        headerRow.createCell(8).setCellValue("Allocation Status Last Modified");
+        headerRow.createCell(9).setCellValue("Comments");
+        headerRow.createCell(10).setCellValue("Created At");
     }
 }
